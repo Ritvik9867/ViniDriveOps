f77141ff6f446cda05aeaa3f996defe0eac9588e
Ritvik9867 <vinitatoursandtravels9920@gmail.com>
2025-04-12 15:08:59 +0530

    26

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:vinidriveops/screens/auth/login_screen.dart';
+import 'package:vini_drive_ops/screens/auth/login_screen.dart';
 
 void main() {
   group('LoginScreen Widget Tests', () {
